023e2a875d9174d4da96ad7606cd917281f361b1
Brandon.F <90413438+Trav1s44@users.noreply.github.com>
2021-11-30 14:26:05 -0500

    v0.2
    Set up game window

--- .github/myfirstpygame.py
+++ .github/myfirstpygame.py
@@ -1,4 +1,12 @@
-#my first pygame, brandon flagg, 11/30/21, 2:19pm, v0.1
+#my first pygame, brandon flagg, 11/30/21, 2:25pm, v0.2
 
 import pygame, sys
 from pygame.locals import *
+
+#start pygame
+pygame.init()
+
+#setup the game window.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello, World!')
+
