7719858958e18d408e63df6b9494d3c75c4180ff
Brandon.F <90413438+Trav1s44@users.noreply.github.com>
2021-11-30 14:47:41 -0500

    v0.4
    setup fount and text

--- .github/myfirstpygame.py
+++ .github/myfirstpygame.py
@@ -1,4 +1,4 @@
-#my first pygame, brandon flagg, 11/30/21, 2:35pm, v0.3
+#my first pygame, brandon flagg, 11/30/21, 2:46pm, v0.4
 
 import pygame, sys
 from pygame.locals import *
@@ -17,3 +17,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLABLU = (55, 150, 255)
+
+# setup fount 
+basicfont = pygame.font.SysFont(None, 48) 
+
+# setup text
+text = basicfont.render('Hello, world!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
