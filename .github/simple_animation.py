d22f08e587f607da8c27cc64c394f1cb473806e6
Brandon.F <90413438+Trav1s44@users.noreply.github.com>
2022-01-06 13:58:35 -0500

    Update simple_animation.py
    imported modules for PyGame system and time.
    
    initialized the PyGame Module.
    
    Setup window dimensions.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with PyGame, Brandon flagg, 1/6/22, 1:44Pm, v0.0
+# Simple Animation with PyGame, Brandon flagg, 1/6/22, 1:55Pm, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# setup pygame
+pygame.init()
+
+# setup the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT),0,32)
+pygame.display.set_caption('Animation Example!')
