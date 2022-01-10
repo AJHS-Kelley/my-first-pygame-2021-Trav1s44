43ad9dde06ee0ba878c90d1b0e221c85d73ee622
Brandon.F <90413438+Trav1s44@users.noreply.github.com>
2022-01-10 13:50:48 -0500

    v0.5
    setup the main loop

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Brandon flagg, 1/10/22, 1:41Pm, v0.4
+# Simple Animation with PyGame, Brandon flagg, 1/10/22, 1:49Pm, v0.5
 
 
 import PyGame, sys, time
@@ -32,3 +32,12 @@ b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
 b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
 b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
 boxes = [b1, b2, b3]
+
+# run the game loop
+while True:
+    #check for Quit event.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
